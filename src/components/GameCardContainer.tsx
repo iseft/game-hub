b9d575c59c232985b6c2660941405a232fd39e46
Istvan Fabian <fabian93915@gmail.com>
2025-01-04 14:27:27 +0100

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,14 @@ interface Props {
 
 export default function GameCardContainer({ children }: Props) {
   return (
-    <Box borderRadius="10px" overflow={"hidden"}>
+    <Box
+      borderRadius="10px"
+      overflow={"hidden"}
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+    >
       {children}
     </Box>
   );
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Text, Heading, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
 
 export default function GameDetailPage() {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 }
